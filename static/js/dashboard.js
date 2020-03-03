14cf2e2916071d356c0c7e5c2a7a169089db7d1c
danaalgot <dana@algot.ca>
2020-03-03 16:55:18 -0700

    Adding Sass

---
+++ static/js/dashboard.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    console.log("ready");
+
+    // $('.menu-icon').on('click', function(){
+    //     $('.site-navigation').toggleClass('menu-toggle');
+    //     $('.menu-icon').toggleClass('icon-closed');
+    //     $('.site-header').toggleClass('menu-closed-header-style ');
+    // });
+});
+
