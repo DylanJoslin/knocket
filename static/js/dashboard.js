a044bfaab85ebcf0cadc2e3f83d27fa3d7762d17
danaalgot <dana@algot.ca>
2020-03-03 20:27:25 -0700

    Creating the Navigation

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 
-    console.log("ready");
+    $('.menu-icon').on('click', function(){
+        $('.dashboard-navigation').toggleClass('menu-toggle');
+        $('.menu-icon').toggleClass('icon-closed');
+        $('.dashboard-header').toggleClass('menu-closed-header-style ');
+    });
 
-    // $('.menu-icon').on('click', function(){
-    //     $('.site-navigation').toggleClass('menu-toggle');
-    //     $('.menu-icon').toggleClass('icon-closed');
-    //     $('.site-header').toggleClass('menu-closed-header-style ');
-    // });
 });
 
