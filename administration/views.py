3144eb2c88389170c7e945af7702e70af768abe3
Tristan Hampton <tristanhampton@icloud.cmo>
2020-03-04 11:15:47 -0700

    Admin page loads pennding students that need approval

--- administration/views.py
+++ administration/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 def admin_home(request): 
-    return render(request, 'administration/admin_home.html')
+
+    context = {
+        'users': User.objects.all()
+    }
+
+    return render(request, 'administration/admin_home.html', context)
