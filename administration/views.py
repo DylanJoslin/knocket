f7de97ffb70e9b8cfc96e25b3f5fb875adcd58fe
Tristan Hampton <tristanhampton@icloud.cmo>
2020-03-11 10:49:54 -0600

    small updates

--- administration/views.py
+++ administration/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import AccessForm
 
 
 # Create your views here.
@@ -26,14 +25,6 @@ def admin_home(request, access='pending'):
     }
     
     return render(request, 'administration/admin_home.html', context)
-
-    # if request.method == 'POST':
-    #     access_form = AccessForm(request.POST, instance=request.user)
-    #     if form.is_valid():
-    #         access = access_form.save(commit=false)
-    #         # user_access.user
-
-    # return render(request, 'administration/admin_home.html', context)
 
 def admin_users(request):
     return render(request, 'administration/admin_users.html')
