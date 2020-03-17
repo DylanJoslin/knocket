ee46183926a91c769d6b329fd7f3730b59ec056b
ssarmientogarci1 <55854531+ssarmientogarci1@users.noreply.github.com>
2020-03-17 12:07:08 -0600

    navigation working in all pages now

--- static/js/main.js
+++ static/js/main.js
@@ -80,23 +80,3 @@ document.querySelector(".prev").addEventListener("click", function(){
         document.querySelector(".mobile-provice-name").innerHTML = "Manitoba";
     }
 })
-
-const menuBtn = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
-const close = document.querySelector('.close-nav');
-const dropdown = document.querySelector('.dropdown');
-const subMenu = document.querySelector('.sub-menu');
-
-menuBtn.addEventListener('click', function(){
-    nav.classList.toggle('show-nav');
-});
-
-dropdown.addEventListener('click', function(){
-    subMenu.classList.toggle('show-sub-menu');
-    subMenu.classList.toggle('sub-menu')
-});
-
-
-close.addEventListener('click', function(){
-    nav.classList.remove('show-nav');
-});
