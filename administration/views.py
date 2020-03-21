834a9bc3b40779fe6036eaeeefa06fc1c00ece67
Tristan Hampton <tristanhampton@icloud.cmo>
2020-03-21 14:43:03 -0600

    admin can create users

--- administration/views.py
+++ administration/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from users.forms import RegistrationForm, UserProfileForm
 
 
 # Create your views here.
@@ -26,17 +27,68 @@ def admin_home(request, access='pending'):
 
     return render(request, 'administration/admin_home.html', context)
 
-def admin_users(request):
-    return render(request, 'administration/admin_users.html')
+def admin_users(request, access='pending'):
+    # Count users to pass into context
+    new_users = 0
+    registered_users = 0
+    for user in User.objects.all():
+        if user.userprofile.access == 'pending':
+            new_users = new_users + 1
+        if user.userprofile.access == 'student':
+            registered_users = registered_users + 1
 
-def admin_uploads(request):
-    return render(request, 'administration/admin_uploads.html')
+    context = {
+        'users': User.objects.all(),
+        'new_users': new_users,
+        'registered_users': registered_users,
+        'access': access
+    }
+    return render(request, 'administration/admin_users.html', context)
+
+def admin_uploads(request, access='pending'):
+    # Count users to pass into context
+    new_users = 0
+    registered_users = 0
+    for user in User.objects.all():
+        if user.userprofile.access == 'pending':
+            new_users = new_users + 1
+        if user.userprofile.access == 'student':
+            registered_users = registered_users + 1
+
+    context = {
+        'users': User.objects.all(),
+        'new_users': new_users,
+        'registered_users': registered_users,
+        'access': access
+    }
+    return render(request, 'administration/admin_uploads.html', context)
 
 def edit_users(request):
     return render(request, 'administration/edit_user.html')
 
 def create_user(request):
-    return render(request, 'administration/create_user.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            user.userprofile.access = 'student'
+            user.userprofile.save()
+            messages.success(request, f'The students account has been created.')
+            return redirect('admin_users')
+    else: 
+        form = RegistrationForm()
+        profile_form = UserProfileForm()
+
+    context = {
+        'form': form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'administration/create_user.html', context)
 
 
 
@@ -59,7 +111,7 @@ def approve_user(request, username='none'):
             user.userprofile.access = 'student'
             user.userprofile.save()
             messages.success(request, f'This student has been approved!')
-            return render(request, 'administration/admin_home.html')
+            return redirect('admin_home')
         else:
             messages.success(request, f'Cannot approve user!')
             return render(request, 'administration/admin_home.html')
