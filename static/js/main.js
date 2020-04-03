e9c3b0841006e5deeddfa162fc93d17e73506f9f
Dylan Joslin <47988968+DylanJoslin@users.noreply.github.com>
2020-04-02 20:39:00 -0600

    Added final version of map - bugs expected

--- static/js/main.js
+++ static/js/main.js
@@ -11,7 +11,7 @@ alberta.addEventListener("mouseover", function(){
 }); // End Alberta Hover Tile Changer
 
 //Mobile Map Slideshow
-let images = ["static/img/bc-final.png", "static/img/alberta-final.png"];
+let images = ["static/img/bc-final.png", "static/img/alberta-final.png", "static/img/sk-final.png", "static/img/mb-final.png", "static/img/on-final.png", "static/img/qc-final.png", "static/img/nl-final.png", "static/img/nb-final.png", "static/img/ns-final.png"];
 let currentImg = 0;
 
 document.querySelector(".next").addEventListener("click", function(){
@@ -31,9 +31,24 @@ document.querySelector(".next").addEventListener("click", function(){
     } else if (currentImg == 2){
         document.querySelector(".mobile-local-button").href = "#";
         document.querySelector(".mobile-provice-name").innerHTML = "Saskatchewan";
-    } else {
+    } else if (currentImg == 3) {
         document.querySelector(".mobile-local-button").href = "#";
         document.querySelector(".mobile-provice-name").innerHTML = "Manitoba";
+    } else if (currentImg == 4) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "Ontario";
+    } else if (currentImg == 5) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "Quebec";
+    } else if (currentImg == 6) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "Newfoundland & Labrador";
+    } else if (currentImg == 7) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "New Brunswick";
+    } else if (currentImg == 8) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "Nova Scotia & Prince Edward Island";
     }
 })
 
@@ -46,16 +61,31 @@ document.querySelector(".prev").addEventListener("click", function(){
     document.querySelector(".holder-mobile>div>img").src = images[currentImg];
 
     if(currentImg == 0){
-        document.querySelector(".mobile-local-button").href = "local-bc.html";
+        document.querySelector(".mobile-local-button").href = "#";
         document.querySelector(".mobile-provice-name").innerHTML = "British Columbia";
     } else if (currentImg == 1){
-        document.querySelector(".mobile-local-button").href = "local-alberta.html";
+        document.querySelector(".mobile-local-button").href = "province/ab";
         document.querySelector(".mobile-provice-name").innerHTML = "Alberta";
     } else if (currentImg == 2){
-        document.querySelector(".mobile-local-button").href = "local-sask.html";
+        document.querySelector(".mobile-local-button").href = "#";
         document.querySelector(".mobile-provice-name").innerHTML = "Saskatchewan";
-    } else {
-        document.querySelector(".mobile-local-button").href = "local-manitoba.html";
+    } else  if (currentImg == 3){
+        document.querySelector(".mobile-local-button").href = "#";
         document.querySelector(".mobile-provice-name").innerHTML = "Manitoba";
+    } else if (currentImg == 4) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "Ontario";
+    } else if (currentImg == 5) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "Quebec";
+    } else if (currentImg == 6) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "Newfoundland & Labrador";
+    } else if (currentImg == 7) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "New Brunswick";
+    } else if (currentImg == 8) {
+        document.querySelector(".mobile-local-button").href = "#";
+        document.querySelector(".mobile-provice-name").innerHTML = "Nova Scotia & Prince Edward Island";
     }
 })
